1bd911d7e87ea32b99df73d7e3662e358e78b531
Sarath Chandra Godithi <sarathchandragodithi@gmail.com>
2023-11-15 21:38:19 +0530

    feat: Modify game resolution

--- src/main/java/stickhero/HelloApplication.java
+++ src/main/java/stickhero/HelloApplication.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Button.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
--- src/main/java/stickhero/HelloController.java
+++ src/main/java/stickhero/HelloController.java
@@ -5,10 +5,10 @@ import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label playButton;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        playButton.setText("Welcome to JavaFX Application!");
     }
 }
