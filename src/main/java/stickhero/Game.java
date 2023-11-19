ca2c40f8e10f530b8b4892ea0a362cfafd698eb7
Sarath Chandra Godithi <sarathchandragodithi@gmail.com>
2023-11-19 17:40:26 +0530

    Revert "refactor: Create main class; Swap main & game class"
    This reverts commit bc4b4ba29d11668cbcb9e5e73442ef89f7cbfeda.

--- src/main/java/stickhero/Game.java
+++ src/main/java/stickhero/Game.java
@@ -1,6 +1,25 @@
 package stickhero;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.io.Serializable;
 
-public class Game implements Serializable {
+public class Game extends Application implements Serializable {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Game.class.getResource("Button.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 }
