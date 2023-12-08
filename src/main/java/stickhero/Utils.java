d181ebd94eb4c81e8d8db14fbc6b407de3318d73
Vedang Patel <pvedang388@gmail.com>
2023-12-08 14:04:09 +0530

    feat: add base pillar

--- src/main/java/stickhero/Utils.java
+++ src/main/java/stickhero/Utils.java
@@ -4,10 +4,18 @@ import javafx.scene.layout.Pane;
 
 public final class Utils {
     private static Pane currentPane;
+    private static Pillar basePillar;
     public static void setPane(Pane pane) {
         currentPane = pane;
     }
     public static Pane getPane() {
         return currentPane;
+    }
+    public static void setBasePillar(Pillar pillar) {
+        basePillar = pillar;
+    }
+
+    public static Pillar getBasePillar() {
+        return basePillar;
     }
 }
