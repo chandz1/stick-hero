b3e28581cad5cb6ac46d5fba3fff090eb4286201
Vedang Patel <pvedang388@gmail.com>
2023-12-10 02:34:56 +0530

    docs: Add PillarSaver comments

--- src/main/java/stickhero/PillarSaver.java
+++ src/main/java/stickhero/PillarSaver.java
@@ -2,27 +2,39 @@ package stickhero;
 
 import java.io.Serializable;
 
+// PillarSaver is Serializable as it needs to written to a file
 public class PillarSaver implements Serializable {
     private final double x;
     private final double width;
     private final double bonusZoneX;
     private final double cherryX;
+
+    // creates a pillar saver
     public PillarSaver(Pillar pillar) {
+        // gets the x position and with of the pillar
         this.x = pillar.getCurrentX();
         this.width = pillar.getWidth();
+
+        // checks if pillar bonus zone exists
         if (pillar.getBonusZone() != null) {
+            // gets the x position of the bonus zone
             this.bonusZoneX = pillar.getBonusZone().getCurrentX();
         } else {
-            this.bonusZoneX = 600;
+            // spawns bonus zone off-screen
+            this.bonusZoneX = -50;
         }
+        // checks if pillar cherry exists
         if (pillar.getCherry() != null) {
+            // gets the x position of cherry
             this.cherryX = pillar.getCherry().getCurrentX();
         } else {
+            // spawns cherry off-screen
             this.cherryX = 600;
         }
 
     }
 
+    // creates a pillar based on the saved values
     public void createPillar(boolean isBase) {
         new Pillar(isBase, this.x, this.width, this.bonusZoneX, this.cherryX);
     }
