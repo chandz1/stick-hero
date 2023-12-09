f672e5a85c08b547ad81f690ed6aea611ec03d31
Vedang Patel <pvedang388@gmail.com>
2023-12-10 02:43:08 +0530

    docs: Add SaveManager comments

--- src/main/java/stickhero/SaveManager.java
+++ src/main/java/stickhero/SaveManager.java
@@ -4,56 +4,64 @@ import javafx.scene.control.Button;
 
 import java.io.*;
 
+// SaveManager is a Singleton class which is Serializable
 public final class SaveManager implements Serializable {
-
     private static SaveManager saveManager = null;
+    private PillarSaver basePillar;
+    private PillarSaver nextPillar;
+    private Score score;
 
-    private PillarSaver basePillar = null;
-    private PillarSaver nextPillar = null;
-    private Score score = null;
-
+    // constructor for save manager is private
     private SaveManager() {
+        this.basePillar = null;
+        this.nextPillar = null;
+        this.score = null;
     }
 
+    // sets the current state of the game
     private void setCurrentState() {
+        // pillar savers are created for the base pillar and next pillar
         basePillar = new PillarSaver(Utils.getBasePillar());
         nextPillar = new PillarSaver(Utils.getNextPillar());
+        // score assigned
         score = Utils.getScore();
     }
 
+
+    // gets the singleton instance of save manager
     public static SaveManager getInstance() {
+        // if save manager doesn't exist
         if (saveManager == null) {
+            // save manager is created
             saveManager = new SaveManager();
         }
+        // singleton object is returned
         return saveManager;
     }
 
+    // saves the current state of the game
     public void save() throws IOException {
-        ObjectOutputStream saveFile = null;
-
-        try {
-            saveFile = new ObjectOutputStream(new FileOutputStream("save.dat"));
+        // tries to write to save.dat
+        try (ObjectOutputStream saveFile = new ObjectOutputStream(new FileOutputStream("save.dat"))) {
+            // sets the current state of the game to save manager
             saveManager.setCurrentState();
+            // writes all the save manager to save.dat
             saveFile.writeObject(saveManager);
-        } finally {
-            if (saveFile != null) {
-                saveFile.close();
-            }
         }
     }
-    public void load() throws IOException, ClassNotFoundException {
-        ObjectInputStream loadFile = null;
 
-        try {
-            loadFile = new ObjectInputStream(new FileInputStream("save.dat"));
+    // loads the current state of the game
+    public void load() throws IOException, ClassNotFoundException {
+        // tries to read save.dat
+        try (ObjectInputStream loadFile = new ObjectInputStream(new FileInputStream("save.dat"))) {
+            // reads the save manager in the save.dat file
             saveManager = (SaveManager) loadFile.readObject();
-        } finally {
-            if (loadFile != null) {
-                loadFile.close();
-            }
         }
+        // creates the base pillar
         saveManager.basePillar.createPillar(true);
+        // creates the next pillar
         saveManager.nextPillar.createPillar(false);
+        // sets the score
         saveManager.score.setScore();
     }
 }
