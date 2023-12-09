5f62863ab90f4af0ba5563350095e2e88baa900d
Vedang Patel <pvedang388@gmail.com>
2023-12-10 01:03:22 +0530

    docs: add comment to Movable

--- src/main/java/stickhero/Movable.java
+++ src/main/java/stickhero/Movable.java
@@ -3,5 +3,6 @@ package stickhero;
 import javafx.animation.TranslateTransition;
 
 public interface Movable {
+    // a function the move the movable interface
     public TranslateTransition move(double x);
 }
