26768dd25dc22270828bc973ec852049c5f4be69
Vedang Patel <pvedang388@gmail.com>
2023-12-10 02:57:28 +0530

    docs: Add Utils comments

--- src/main/java/stickhero/Utils.java
+++ src/main/java/stickhero/Utils.java
@@ -19,59 +19,76 @@ public final class Utils {
 
     private static Score score;
 
+    // gets whether invisible mode is on
     public static boolean isInvisibleMode() {
         return invisibleMode;
     }
 
+    // sets the invisible mode
     public static void setInvisibleMode(boolean invisibleMode) {
         Utils.invisibleMode = invisibleMode;
     }
 
+    // sets the score
     public static void setScore(Score score) {
         Utils.score = score;
     }
 
+    // gets the score
     public static Score getScore() {
         return score;
     }
 
+    // gets the hero
     public static Hero getHero() {
         return hero;
     }
 
+    // sets the hero
     public static void setHero(Hero hero) {
         Utils.hero = hero;
     }
 
+    // sets the pane
     public static void setPane(Pane pane) {
         currentPane = pane;
     }
+
+    // gets the pane
     public static Pane getPane() {
         return currentPane;
     }
+
+    // sets the base pillar
     public static void setBasePillar(Pillar pillar) {
         basePillar = pillar;
     }
 
+    // gets the base pillar
     public static Pillar getBasePillar() {
         return basePillar;
     }
 
+    // sets the current scene
     public static void setCurrentScene(Scene scene) {
         currentScene = scene;
     }
 
+    // gets the current scene
     public static Scene getCurrentScene() {
         return currentScene;
     }
 
+    // sets the next pillar
     public static void setNextPillar(Pillar nextPillar) {
         Utils.nextPillar = nextPillar;
     }
 
+    // gets the next pillar
     public static Pillar getNextPillar() {
         return nextPillar;
     }
 
+    // looks up the node in the pane and returns it
     public static Node paneLookup(String lookup) { return getPane().lookup(lookup); }
 }
