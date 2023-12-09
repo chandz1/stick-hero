db493f1fe89beb12c3c0e7ef558f96ebac4298a2
Vedang Patel <pvedang388@gmail.com>
2023-12-10 01:58:32 +0530

    docs: Add MenuController comments

--- src/main/java/stickhero/MenuController.java
+++ src/main/java/stickhero/MenuController.java
@@ -28,52 +28,75 @@ public class MenuController implements Initializable {
     @FXML
     private CheckBox invisibleCheck;
 
-
+    // the function is called when the new game button is pressed
+    @FXML
     public void newGame() throws IOException {
+        // invisible mode is set based on its check box
         Utils.setInvisibleMode(invisibleCheck.isSelected());
 
+        // loads a new fxml
         Pane pane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Game.fxml")));
+        // removes everything from the pane and sets all the children as the pane from the fxml
         root.getChildren().setAll(pane);
+        // sets the root as current pane
         Utils.setPane(root);
+
+        // creates a new base pillar
         Pillar pillar = new Pillar(true);
 
+        // creates a new hero
         Hero hero = new Hero();
         Utils.setHero(hero);
 
+        // next pillar is created a brought to screen
         Pillar pillar1 = new Pillar(false);
         pillar1.bringToScreen().play();
 
+        // a new score is created
         Score score = new Score();
         Utils.setScore(score);
 
+        // the score should all be set to 0 by default. this is redundant code
         Utils.getScore().updateScore();
-
-        GameController gameController = new GameController();
-        gameController.controlGame();
     }
 
+    // the function is called when the continue button is pressed
+    @FXML
     public void continueGame() throws IOException, ClassNotFoundException {
+        // loads a new fxml
         Pane pane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Game.fxml")));
+        // removes everything from the pane and sets all the children as the pane from the fxml
         root.getChildren().setAll(pane);
+        // sets the root as current pane
         Utils.setPane(root);
+
+        // loads the save
         SaveManager.getInstance().load();
+
+        // creates a hero
         Hero hero = new Hero();
         Utils.setHero(hero);
-        Utils.getScore().updateScore();
 
-        GameController gameController = new GameController();
-        gameController.controlGame();
+        // the score is updated
+        Utils.getScore().updateScore();
     }
 
+    // the function is called when the quit button is pressed
+    @FXML
     public void quit() {
+        // exits the javafx
         exit();
     }
 
+    // the function is called when the controller is initialized
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        // continue button and quit button will no longer have focus
         quitButton.setFocusTraversable(false);
         continueButton.setFocusTraversable(false);
+        // checks if save data exists
         if (!new File("save.dat").exists()) {
+            // disable continuing if the save data doesn't exist
             continueButton.setDisable(true);
         }
     }
